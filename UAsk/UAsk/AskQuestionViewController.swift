9678a80afd9340980d5fb9292cdb077b95452ff1
Megan <megan.farleigh@outlook.com>
2019-06-01 18:03:58 +1000

    Added create question and added launch text

--- UAsk/UAsk/AskQuestionViewController.swift
+++ UAsk/UAsk/AskQuestionViewController.swift
@@ -7,15 +7,92 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
-class AskQuestionViewController: UIViewController {
-
+class AskQuestionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    var db: Firestore!
+    
+    @IBOutlet weak var questionTxt: UITextView!
+    @IBOutlet weak var facultyPicker: UIPickerView!
+    @IBOutlet weak var submitQButton: UIButton!
+    @IBOutlet weak var successfulTxt: UILabel!
+    
+    var facultyData = ["Faculty","Engineering and IT", "Arts and Social Sciences", "Design, Architecture and Building", "Law", "Business", "Science", "Health", "Trans-Disciplinary Innovation"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.facultyPicker.dataSource = self
+        self.facultyPicker.delegate = self
+        
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            // Handle authenticated state
+        }
+        
+        // [START setup]
+        let settings = FirestoreSettings()
+        
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
         // Do any additional setup after loading the view.
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return facultyData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return facultyData[row]
+    }
+    
+    @IBAction func submitQuestion(_ sender: Any) {
+        if (checkFieldValues()) {
+            creatQuestionInFirebase()
+            clearField()
+            successfulTxt.isHidden = false
+        }
+    }
+    
+    func checkFieldValues() -> Bool {
+        let selectedValue = facultyData[facultyPicker.selectedRow(inComponent: 0)]
+        
+        guard let question = questionTxt.text, !question.isEmpty else {
+            return false
+        }
+        
+        if (selectedValue == "Faculty") {
+            return false
+        }
+        return true
+    }
+
+    func creatQuestionInFirebase() {
+        var uid = ""
+        let user = Auth.auth().currentUser
+        if let user = user {
+            uid = user.uid
+        }
+        
+        db.collection("questions").addDocument(data: [
+            "faculty": facultyData[facultyPicker.selectedRow(inComponent: 0)],
+            "questionTxt": questionTxt.text,
+            "userId": uid
+            ])
+    }
+    
+    func clearField() {
+        questionTxt.text.removeAll()
+        facultyPicker.reloadAllComponents()
+    }
 
     /*
     // MARK: - Navigation
